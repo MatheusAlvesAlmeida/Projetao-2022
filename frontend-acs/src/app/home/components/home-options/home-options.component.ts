cbea804d20528a34f2974e18289450c19e9bf446
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2022-04-09 11:13:08 -0300

    add: tab de novas solicitacoes

--- frontend-acs/src/app/home/components/home-options/home-options.component.ts
+++ frontend-acs/src/app/home/components/home-options/home-options.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { MatTable } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
+
+export interface suggestionDates {
+  name: string;
+  id: number;
+  service: string;
+  timestamp: string;
+}
+
+const ELEMENT_DATA: suggestionDates[] = [
+  { id: 1, name: 'Gabriel', service: "Urologista", timestamp: '09:00 - 05/05/2022' },
+  { id: 2, name: 'Pietro', service: "Dentista", timestamp: '09:00 - 04/05/2022' },
+];
 
 @Component({
   selector: 'app-home-options',
   templateUrl: './home-options.component.html',
   styleUrls: ['./home-options.component.css']
 })
+
 export class HomeOptionsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  displayedColumns: string[] = ['name', 'service', 'timestamp', 'actions'];
+  columnsToDisplay: string[] = this.displayedColumns.slice();
+  dataSource = [...ELEMENT_DATA];
+
+  confirmAllSuggestions() {
+
+  }
+
+  cancelAllSuggestions() {
   }
 
 }
