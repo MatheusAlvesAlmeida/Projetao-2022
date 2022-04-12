5c5c5e1562dc7ffa668c70d92694c219f54feca8
alps2 <alps2@cin.ufpe.br>
2022-04-12 10:44:02 -0300

    Feat: Make appointment flux

--- bot/speeches.py
+++ bot/speeches.py
@@ -6,6 +6,7 @@ denial_speech = dict()
 check_appointment_speech = dict()
 reschedule_speech = dict()
 cancel_speech = dict()
+error_speech = dict()
 
 #Registering a new user
 register_speech['hello'] = "Parece que é a primeira vez que você usa os serviços do atendente virtual, então vamos precisar fazer um rápido cadastro para que eu possa te conhecer melhor! Primeiro, qual o seu nome completo?"
@@ -22,7 +23,7 @@ users_speech['invalid'] = "Opção inválida."
 
 
 #Make an appointment
-appointment_speech['specialty'] = "Para qual especialidade você gostaria de marcar consulta? <especialidades>"
+appointment_speech['specialty'] = "Para qual especialidade você gostaria de marcar consulta?"
 appointment_speech['date_time'] = "Esses são os próximos 10 horários disponíveis para <especialidade_escolhida> em <nome_UBS>. Escolha o que mais lhe agrada: <Horários>"
 appointment_speech['user_confirmation'] = "Você confirma que deseja marcar uma consulta para <especialidade_escolhida>, na <nome_UBS>, no seguinte horário: <horário_escolhido>? \n1) Sim. \n2) Não."
 appointment_speech['apointment_ending'] = "Seu pedido de agendamento de consulta foi encaminhado para o ACS responsável. Assim que ele processar seu pedido, eu te avisarei!"
@@ -48,3 +49,6 @@ reschedule_speech['acs_confirmation'] = "O ACS responsável confirmou o seu reag
 cancel_speech['specialty'] = "Para qual especialidade você gostaria de reagendar a consulta? <especialidades com agendamento>"
 cancel_speech['user_confirmation'] = "Você confirma que deseja cancelar uma consulta para <especialidade_escolhida>, na <nome_UBS>, no seguinte horário: <horário_escolhido> do dia: <dia_escolhido>? \n 1) Sim 2) Não "
 cancel_speech['acs_notified']  = "Seu pedido de cancelamento de consulta foi encaminhado para ACS responsável."
+
+error_speech['invalid_number'] = "Inválido. Pode repetir? Dessa vez, escolha apenas um número das opções listadas anteriormente."
+error_speech['only_numbers'] = "Pode repetir? Dessa vez, apenas o número, por favor!"
