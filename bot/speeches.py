84fe152c574a78e6ee537493a44ec17897f886b5
pbsm <pietromasur@hotmail.com>
2022-04-14 20:10:06 -0300

    [FIXED] string formatting

--- bot/speeches.py
+++ bot/speeches.py
@@ -17,7 +17,7 @@ register_speech['success'] = "Pronto, seu cadastro foi realizado para os serviç
 register_speech['failure'] = "Infelizmente não foi possível realizar o seu cadastro nesse momento. Tente novamente mais tarde."
 
 #Registered useres
-users_speech['hello'] = "Olá %s! Em que posso ajudar?\n1) Solicitar agendamento.\n2) Solicitar reagendamento.\n3) Solicitar cancelamento.\n4) Consultar agendamentos. \n5) Conversar com o meu ACS. \n6) Nada."
+users_speech['hello'] = "Olá {}! Em que posso ajudar?\n1) Solicitar agendamento.\n2) Solicitar reagendamento.\n3) Solicitar cancelamento.\n4) Consultar agendamentos. \n5) Conversar com o meu ACS. \n6) Nada."
 users_speech['acs_notified'] = "O ACS responsável foi notificado e entrará em contato com você em breve!"
 users_speech['end'] = "Então até a próxima!"
 users_speech['invalid'] = "Opção inválida."
@@ -26,7 +26,7 @@ users_speech['invalid'] = "Opção inválida."
 #Make an appointment
 appointment_speech['specialty'] = "Para qual especialidade você gostaria de marcar consulta?"
 #<especialidade>, <nome_ubs>, <horarios>
-appointment_speech['date_time'] = "Esses são os próximos 10 horários disponíveis para {} em {}. Escolha o que mais lhe agrada: {}"
+appointment_speech['date_time'] = "Esses são os próximos 10 horários disponíveis para {} em {}. Escolha o que mais lhe agrada:"
 #<especialidade>, <nome ubs>, <horário escolhido>
 appointment_speech['user_confirmation'] = "Você confirma que deseja marcar uma consulta para {}, na {}, no seguinte horário: {}? \n1) Sim. \n2) Não."
 appointment_speech['apointment_ending'] = "Seu pedido de agendamento de consulta foi encaminhado para o ACS responsável. Assim que ele processar seu pedido, eu te avisarei!"
@@ -42,17 +42,6 @@ denial_speech['acs_notified'] = "O ACS responsável foi notificado e entrará em
 #Check appointments
 check_appointment_speech['confirmed'] = "Esses são os seus agendamentos confirmados:"
 check_appointment_speech['pending']= "Esses são os seus agendamentos ainda pendentes:"
-
-#Reschedule appointment
-#<especialidades com agendamento>
-reschedule_speech['specialty'] = "Para qual especialidade você gostaria de reagendar a consulta? {}"
-#<especialidade>, <nome_UBS>,<horarios>
-reschedule_speech['date_time'] = "Esses são os próximos 10 horários disponíveis para {} em {}: {}"
-#<especialidade>, <nome_UBS>,<horario_selecionado>
-reschedule_speech['user_confirmation']= "Você confirma que deseja solicitar remarcação de consulta para {}, na {}, no seguinte horário: {} ? \n1) Sim \n2) Não"
-reschedule_speech['request_sent'] = "Seu pedido de agendamento de consulta foi encaminhado para o ACS responsável. Assim que ele processar seu pedido, eu te avisarei!"
-#<link google calendar>
-reschedule_speech['acs_confirmation'] = "O ACS responsável confirmou o seu reagendamento. Fique atento para as datas e não se esqueça de solicitar o reagendamento caso não seja possível comparecer no dia. Use esse link para adicionar a consulta como evento ao seu calendário: {}"
 
 #Cancel appointment
 #<especialidade_escolhida>, <nome_UBS>, <especialidade_escolhida>, <dia_escolhido>
