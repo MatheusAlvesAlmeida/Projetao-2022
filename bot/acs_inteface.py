5c4fe4ab77bda1e2c75491167118324a43633758
alps2 <alps2@cin.ufpe.br>
2022-04-13 23:18:13 -0300

    Feat: Interface classes hold endpoints functions

---
+++ bot/acs_inteface.py
@@ -0,0 +1,15 @@
+class AcsFunctions:
+    
+    @staticmethod
+    def notify_acs_contact(user_infos: dict):
+        """
+        Receives a dict containing all the information needed by the ACS to contact the user.
+        Sends the request to the ACS contact requests list interface.
+        """
+    
+    @staticmethod
+    def notify_acs_appointment(appointment_infos: dict):
+        """
+        Receives a dict containing all the information needed by the ACS to make an appointment.
+        Sends the request to the ACS appointment requests list interface.
+        """
