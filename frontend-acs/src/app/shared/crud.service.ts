1c929f656253ba9c5a0894e7fef2c94dc0809a73
gabrielnogueiralt <gabrielnogueiraleite@gmail.com>
2022-05-05 00:30:57 -0300

    creates the crud file to interate with firebase database

---
+++ frontend-acs/src/app/shared/crud.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Pending } from '../home/types/pending';
+import { ConfirmedPatients } from '../home/types/confirmed-patients';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+  AngularFireObject,
+} from '@angular/fire/compat/database';
+@Injectable({
+  providedIn: 'root',
+})
+
+
+export class CrudService {
+  // TODO: fix this error
+  patientsRef: AngularFireList<any>;
+  patientRef: AngularFireObject<any>;
+
+  constructor(private db: AngularFireDatabase) {}
+
+  // Create Confirmed Patients List
+  addConfirmedQueue(confirmedPatients: ConfirmedPatients) {
+    this.patientsRef.push({
+      confirmedPatients
+    });
+  }
+
+  // Fetch Pending Patients List
+  getPendingList() {
+    this.patientsRef = this.db.list('pendentes');
+    return this.patientsRef;
+  }
+
+  // Delete Student Object
+  deletePending(cadastro_sus: string) {
+    this.patientRef = this.db.object('pendentes/' + cadastro_sus);
+    this.patientRef.remove();
+  }
+}
